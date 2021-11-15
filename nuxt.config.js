4427eddf00ed2b889c9dcfa0d4915bada5d94a57
Nauras Jabari <nauras@agencyundone.com>
2021-11-15 13:44:22 -0800

    Feat: set up static site Nuxt boilerplate

---
+++ nuxt.config.js
@@ -0,0 +1,142 @@
+export default {
+  // //////////////////////////////////////////// Static Site Generation Options
+  // ---------------------------------------------------------------------------
+  target: 'static',
+  // ///////////////////////////////////////////////////// Runtime Configuration
+  // ---------------------------------------------------------------------------
+  // ---------------------------------------------------------- [Runtime] Public
+  publicRuntimeConfig: {
+    nodeEnv: process.env.NODE_ENV,
+    serverEnv: process.env.SERVER_ENV,
+    seo: {
+      siteName: 'Filecoin Foundation for the Decentralized Web',
+      siteUrl: ''
+    },
+    redirects: []
+  },
+  // --------------------------------------------------------- [Runtime] Private
+  privateRuntimeConfig: {},
+  // /////////////////////////////////////////////////////////// Server & Render
+  // ---------------------------------------------------------------------------
+  server: {
+    port: 25000,
+    host: process.env.NODE_ENV !== 'development' ? '0.0.0.0' : 'localhost'
+  },
+  render: {
+    bundleRenderer: {
+      runInNewContext: false
+    }
+  },
+  // /////////////////////////////////////////////////////// Headers of the Page
+  // ---------------------------------------------------------------------------
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' }
+    ]
+  },
+  // ////////////////////////////////////////// Customize the progress-bar color
+  // ---------------------------------------------------------------------------
+  loading: {
+    color: '#000000',
+    height: '4px'
+  },
+  // /////////////////////////////////////////////////////////// Global CSS/SCSS
+  // ---------------------------------------------------------------------------
+  css: [
+    '~/assets/scss/main.scss'
+  ],
+  styleResources: {
+    scss: [
+      '~/assets/scss/variables.scss'
+    ]
+  },
+  // /////////////////////////////////////////////////////// Nuxt.js Dev Modules
+  // ---------------------------------------------------------------------------
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    // Doc: https://github.com/nuxt-community/moment-module#readme
+    '@nuxtjs/moment'
+  ],
+  // /////////////////////////////////////////////////////////// Nuxt.js Modules
+  // ---------------------------------------------------------------------------
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    // Doc: https://github.com/nuxt-community/style-resources-module
+    '@nuxtjs/style-resources',
+    // Doc: https://github.com/agency-undone/au-nuxt-module-zero
+    'au-nuxt-module-zero',
+    // Doc: https://sitemap.nuxtjs.org
+    // '@nuxtjs/sitemap',
+    // Doc: https://content.nuxtjs.org
+    '@nuxt/content'
+  ],
+  // ///////////////////////////////////////////////////// [Module] Nuxt-content
+  // ---------------------------------------------------------------------------
+  content: {
+    markdown: {
+      prism: {
+        theme: false
+      }
+    }
+  },
+  // ///////////////////////////////////////////////////////////// [Module] Zero
+  // ---------------------------------------------------------------------------
+  zero: {
+    core: {
+      include: true,
+      toaster: {
+        display: 10,
+        timeout: 5000
+      }
+    },
+    filters: {
+      include: true
+    },
+    pagination: {
+      include: true
+    }
+  },
+  // ///////////////////////////////////////////////////////// [Module] MomentJS
+  // ---------------------- Doc: https://github.com/nuxt-community/moment-module
+  moment: {
+    timezone: true,
+    defaultTimezone: 'UTC'
+  },
+  // //////////////////////////////////////////////////////////// [Module] Axios
+  // -------------------------------------- See https://axios.nuxtjs.org/options
+  axios: {},
+  // ///////////////////////////////////////////////////////////// [Module] Zero
+  // ------------------------------------------- Dox: https://sitemap.nuxtjs.org
+  sitemap: {
+    hostname: '/'
+  },
+  // /////////////////////////////////// Plugins to load before mounting the App
+  // ---------------------------------------------------------------------------
+  plugins: [],
+  // /////////////////////////////////////////////////////// Router + Middleware
+  // ---------------------------------------------------------------------------
+  router: {},
+  // /////////////////////////////////////////////////////// Build configuration
+  // ------------------------------------------------ Extend webpack config here
+  build: {
+    // ---------------------------------------------------------- Hot Middleware
+    hotMiddleware: {
+      client: {
+        overlay: false
+      }
+    },
+    // -------------------------------------------------------------- Extensions
+    extend (config, ctx) {
+      config.module.rules.push({
+        test: /\.md$/,
+        use: 'raw-loader'
+      })
+    }
+  }
+}
--- readme.md
+++ readme.md
@@ -1,20 +1,171 @@
-## Overview
+# Filecoin Foundation for the Decentralized Web
 
-This is an early version and temporary site for the Filecoin Foundation for the Decentralized Web (FFDW). 
+Website for the Filecoin Foundation for the Decentralized Web
 
-In the interest of time, this current version simply reflects the design and is implemented with a single SVG object, and minimal interactivity. This implementation is designed to only be a stopgap, and will need to be updated in favor of an accessible, portable, and extensible alternative.
+## Components
 
-## Build
+**Example Data Structure**
 
-- `develop` is the staging branch which deploys to [staging-mmfpfrmycb.ffdweb.org](//staging-mmfpfrmycb.ffdweb.org)
-- `main` is the production branch which deploys to [ffdweb.org](//ffdweb.org)
+```json
+{
+  "id": "intro_1",
+  "grid": ["middle"],
 
-No build necessary at the moment. This is a simple static site requiring no compilation.
+  "left": {
+    "type": "video_block",
+    "cols": {
+      "num": "col-6",
+      "push_left": "off-2"
+    },
+    "preview_image": "/images/placeholder-image.png",
+    "url": "https://www.youtube.com/watch?v=wP4Bk8lBNUc"
+  },
+  "right": {
+    "type": "text_block",
+    "layout": "compact",
+    "cols": {
+      "num": "col-4",
+      "push_left": "off-3"
+    },
+    "label": {
+      "type": "D",
+      "theme": "light",
+      "action": "video",
+      "icon": "play",
+      "text": "Watch now",
+      "url": "https://www.youtube.com/watch?v=wP4Bk8lBNUc"
+    },
+    "description": "Join us as Thomas Jefferson argues with Alexander Hamilton in Washington D.C.",
+    "customizations": [
+      {
+        "name": "BackgroundLayers",
+        "props": {
+          "layers-array": [8, 6, 5, 4, 2, 1],
+          "offset": 1.375,
+          "reverse": true
+        }
+      }
+    ]
+  }
+}
+```
 
-Eventually this will be turned into a `nuxt` static app.
+**Common Keys**
 
-## Content
+`grid` | this is a top-level section key. Any strings in this array will get _attached_ to the `grid` class.
 
-At this time, content is not abstracted from the SVG object.
+```js
+{
+  grid: [String]
+}
+```
 
-The content within the `develop` branch is treated as not indexable by search, so a `noindex, nofollow` attribute is used. This value should not be retained for the production site, or the `main` branch.
+`left`, `right` | these keys contain all the content for the left or right columns. Only 1 is required.
+
+```js
+{
+  left: Object,
+  right: Object
+}
+```
+
+`cols` | this key is nested inside the `left` or `right` keys
+
+```js
+{
+  cols: {
+    num: String,
+    push_left: String,
+    push_right: String
+  }
+}
+```
+
+**Text Block** | `text_block`
+
+```js
+{
+  type: 'text_block',
+  layout: String, // 'large', 'medium' or 'compact'
+  label: String|<Button>, // String → simply outputs string | Object → <Button> component is outputted
+  heading: String,
+  subheading: String,
+  description: String,
+  ctas: [<Button>]
+}
+```
+
+**Video Block** | `video_block`
+
+```js
+{
+  type: 'video_block',
+  preview_image: String,
+  url: String,
+  subtext: <Button>,
+  tint: Boolean
+}
+```
+
+**Image Block** | `image_block`
+
+```js
+{
+  type: 'image_block',
+  src: String
+}
+```
+
+**Button**
+
+```js
+{
+  type: String, // letter of the alphabet like 'A' or 'B'
+  theme: String, // 'light' or 'dark'
+  action: String, // 'link', 'nuxt-link', 'button' or 'video'
+  icon: String,
+  text: String,
+  url: String // if action === 'video', this is the URL to the video that opens in the modal
+}
+```
+
+**Icons**
+
+Below is a list of icon values that can be passed into any `icon` property in the component objects:
+
+- `play`
+- `info`
+
+**FloatingMenu**
+
+Below is a list of icon values that can be passed into any `icon` property in the component objects:
+
+```js
+props: {
+  id: String, // id of the component instance (optional)
+  cutoff: String, // id of an element on the page which when reached will cancel the floating effect (required)
+  offsettop: Number, // how far the threshold to enable floating is placed in advance of the viewport top (in pixels) (optional)
+  offsetbottom: Number, // how far the threshold to disable floating is placed in advance of the viewport bottom (in pixels) (optional)
+  content: String, // inline html to be injected as floating content (optional)
+  entries: [ // an array of objects with menu items. The following is an example of the format:
+    {
+      "heading": "Heading 1",
+      "id": "heading-1-id",
+      "items": [ // an array of subheadings
+        {
+          "text": "Subheading 1",
+          "id": "subheading-1-id"
+        },
+        {
+          "text": "Subheading 2",
+          "id": "subheading-2-id"
+        },
+        {
+          "text": "Subheading 3",
+          "id": "subheading-3-id"
+        }
+      ]
+    }
+  ]
+}
+```
