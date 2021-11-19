c5c67851c0490ce7d47ff2b1983e9e9f667ff788
Nauras Jabari <nauras@agencyundone.com>
2021-11-19 13:56:15 -0800

    Feat: built and fine-tuned Home page up to Callout

---
+++ components/HomeCallout.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="get-involved-callout">
+
+    CALLOUT
+
+  </div>
+</template>
+
+<script>
+// ===================================================================== Imports
+// import TextBlock from '@/components/TextBlock'
+
+// ====================================================================== Export
+export default {
+  name: 'HomeCallout',
+
+  components: {
+    // TextBlock
+  },
+
+  props: {
+    img_left: { /* eslint-disable-line */
+      type: [String, Boolean],
+      required: false,
+      default: false
+    },
+    label: {
+      type: [String, Boolean],
+      required: false,
+      default: false
+    },
+    text_1: { /* eslint-disable-line */
+      type: [String, Boolean],
+      required: false,
+      default: false
+    },
+    text_2: { /* eslint-disable-line */
+      type: [String, Boolean],
+      required: false,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+// ///////////////////////////////////////////////////////////////////// General
+</style>
