32e3d885e0bdf7f0b6f63cad1a5b2a9f142e8ca7
Ben Munoz <benmunozj@gmail.com>
2021-12-14 20:57:01 -0500

    feat Contact page and home page forms wired up to FFDW FieldGoal account

--- components/SlateVideoBlock.vue
+++ components/SlateVideoBlock.vue
@@ -40,11 +40,16 @@ export default {
       slateCollections: 'global/slateCollections'
     }),
     videos () {
-      const collection = this.slateCollections.find((col) => { return col.slatename === this.block.slatename })
-      return collection.objects.filter((obj) => { return !obj.url })
+      return []
+      // const collection = this.slateCollections.find((col) => { return col.slatename === this.block.slatename })
+      // return collection.objects.filter((obj) => { return !obj.url })
       // return collection.objects
     }
   }
+  //
+  // mounted () {
+  //   console.log(this.slateCollections)
+  // }
 }
 </script>
 
